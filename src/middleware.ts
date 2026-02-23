4aca612faa4f98f642713d12c160b2ea56220953
Viktor.Gladkich <viktorgladkich7@gmail.com>
2026-02-23 14:04:01 +0100

    enable maintenance mode

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isMaintenanceMode = false;
+  const isMaintenanceMode = true;
   const isDev = process.env.NODE_ENV === "development";
 
   if (isDev || !isMaintenanceMode) {
