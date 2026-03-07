5846e63994cf9391cf3c50f1ef7f783a6157de46
Viktor.Gladkich <viktorgladkich7@gmail.com>
2026-03-07 21:15:10 +0100

    Decrease stroke width of SVG paths in Navbar from 3px to 2px

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -157,7 +157,7 @@ export function Navbar() {
             >
               <path
                 className={cn(
-                  "fill-none stroke-black stroke-[3px] transition-[stroke-dasharray,stroke-dashoffset] duration-[600ms] ease-[cubic-bezier(0.4,0,0.2,1)]",
+                  "fill-none stroke-black stroke-[2px] transition-[stroke-dasharray,stroke-dashoffset] duration-[600ms] ease-[cubic-bezier(0.4,0,0.2,1)]",
                   mobileMenuOpen
                     ? "[stroke-dasharray:20_300] [stroke-dashoffset:-32.42]"
                     : "[stroke-dasharray:12_63]",
@@ -167,7 +167,7 @@ export function Navbar() {
                 d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
               />
               <path
-                className="fill-none stroke-black stroke-[3px] transition-[stroke-dasharray,stroke-dashoffset] duration-[600ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
+                className="fill-none stroke-black stroke-[2px] transition-[stroke-dasharray,stroke-dashoffset] duration-[600ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M7 16 27 16"
